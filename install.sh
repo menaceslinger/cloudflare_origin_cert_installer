c13bdeafca1219938ee03fe1684b65e52199ccff
Saffat Bokul <saffatbokul@gmail.com>
2021-08-13 21:40:21 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo mkdir -p /etc/cloudflare/
+sudo nano /etc/cloudflare/example.com.pem
+sudo nano /etc/cloudflare/example.com.key
+sudo a2enmod ssl
+sudo nano /etc/apache2/sites-available/000-default.conf
