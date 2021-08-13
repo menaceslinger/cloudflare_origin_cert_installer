87aa7192ec079ec802289a0d16709d6537f32fa8
Saffat Bokul <saffatbokul@gmail.com>
2021-08-13 21:42:09 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /etc/cloudflare/
-sudo nano /etc/cloudflare/example.com.pem
-sudo nano /etc/cloudflare/example.com.key
+touch /etc/cloudflare/example.com.pem
+touch /etc/cloudflare/example.com.key
 sudo a2enmod ssl
 sudo nano /etc/apache2/sites-available/000-default.conf
